a8546e8e3be74f6df43c6ca585aaca49c8a3848c
Marylin6 <marylin06@yandex.by>
2026-04-15 10:16:27 +0300

    rename

--- src/main/java/com/javacore/arrays/entity/Observable.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.javacore.arrays.entity;
-
-import main.java.com.javacore.arrays.observer.Observer;
-
-public interface Observable {
-    void addObserver(Observer observer);
-    void notifyObservers();
-}
--- src/main/java/com/javacore/arrays/specification/Specification.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.javacore.arrays.specification;
-
-import main.java.com.javacore.arrays.entity.DoubleArrayEntity;
-
-public interface Specification {
-    boolean specify(DoubleArrayEntity entity);
-}
